ba3b1b792b503c699af389152d43fde5dff49ec9
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-08 14:31:56 +0100

    Add Provider to KanbanBoard

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Boarder from "./Boarder";
 import TaskForm from "./TaskForm";
+import TasksContext from "../context/TasksContext";
+import { initTasks } from './providers/initData';
+
 
 const KanbanBoard = () => {
+    const { Provider } = TasksContext
+
     return (
         <>
             <Header />
-            <Boarder />
-            <TaskForm />
+            <Provider value={{ tasks, moveTask, deleteTask, addTask }}>
+                <Boarder />
+                <TaskForm />
+            </Provider>
         </>
     )
 }
