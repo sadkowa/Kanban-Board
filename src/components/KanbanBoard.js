27b2c660893a225f18de2b48ca1d3954623d34f3
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-08 15:01:09 +0100

    Check develop

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -2,43 +2,43 @@ import React, {useState} from "react";
 import Header from "./Header";
 import Boarder from "./Boarder";
 import TaskForm from "./TaskForm";
-import TasksContext from "../context/TasksContext";
-import { initTasks } from '../providers/initData';
-import { useStorage } from "../hooks";
+// import TasksContext from "../context/TasksContext";
+// import { initTasks } from '../providers/initData';
+// import { useStorage } from "../hooks";
 
 
 const KanbanBoard = () => {
-    const { Provider } = TasksContext
-    const [tasks, setTasks] = useState(initTasks)
+    // const { Provider } = TasksContext
+    // const [tasks, setTasks] = useState(initTasks)
 
-    const [getItem, setItem] = useStorage()
+    // const [getItem, setItem] = useStorage()
 
-    const deleteTask = (id) => {
-        setTasks(prevTasks => {
-            const changedTasks = prevTasks.filter(task => task.id !== id)
-            setItem('tasks', changedTasks)
+    // const deleteTask = (id) => {
+    //     setTasks(prevTasks => {
+    //         const changedTasks = prevTasks.filter(task => task.id !== id)
+    //         setItem('tasks', changedTasks)
 
-            return changedTasks
-        })
-    }
+    //         return changedTasks
+    //     })
+    // }
 
-    const addTask = (task) => {
-        setTasks(prevTasks => {
-            const changedTasks = [...prevTasks, task]
-            setItem('tasks', changedTasks)
+    // const addTask = (task) => {
+    //     setTasks(prevTasks => {
+    //         const changedTasks = [...prevTasks, task]
+    //         setItem('tasks', changedTasks)
 
-            return changedTasks
-        })
-    }
+    //         return changedTasks
+    //     })
+    // }
 
 
     return (
         <>
             <Header />
-            <Provider value={{ tasks, deleteTask, addTask }}>
+            {/* <Provider value={{ tasks, deleteTask, addTask }}> */}
                 <Boarder />
                 <TaskForm />
-            </Provider>
+            {/* </Provider> */}
         </>
     )
 }
