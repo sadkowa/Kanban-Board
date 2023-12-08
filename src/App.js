01d810dbee75e61de07335d1f58dc8bd142fea9d
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-08 14:06:12 +0100

    Create KanbanBoard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import KanbanBoard from './components/KanbanBoard';
 
 function App() {
   return (
---
+++ src/components/KanbanBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const KanbanBoard = () => {
+    return (
+        <>
+            <Header />
+            <Boarder />
+            <TaskForm />
+        </>
+    )
+}
+
+export default KanbanBoard
