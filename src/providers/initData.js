b2cec327bb1302741e11b6201d5192723c38ad53
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-08 14:48:44 +0100

    Change init values

--- src/providers/initData.js
+++ src/providers/initData.js
@@ -1,9 +1,8 @@
 const initColumns = [
-  { id: 1, name: 'Pending', limit: 6 },
-  { id: 2, name: 'Analysis', limit: 3 },
-  { id: 3, name: 'Development', limit: 5 },
-  { id: 4, name: 'Test', limit: 3 },
-  { id: 5, name: 'Deploy', limit: 5 }, 
+  { id: 1, name: 'To do', limit: 6 },
+  { id: 2, name: 'In progress', limit: 3 },
+  { id: 3, name: 'In review', limit: 5 },
+  { id: 4, name: 'Done', limit: 3 } 
   ]
 
   const initTasks = [
@@ -16,10 +15,7 @@ const initColumns = [
     {id: 7, name: 'Task7', idColumn: 3, user: 'Marek'},
     {id: 8, name: 'Task8', idColumn: 3, user: 'Stefan'},
     {id: 10, name: 'Task10', idColumn: 4, user: 'Jakub'},
-    {id: 11, name: 'Task11', idColumn: 4, user: 'Iwona'},
-    {id: 12, name: 'Task12', idColumn: 4, user: 'Mateusz'},
-    {id: 13, name: 'Task13', idColumn: 4, user: 'Jakub'},
-    {id: 16, name: 'Task16', idColumn: 5, user: 'Iwona'},
+    {id: 11, name: 'Task11', idColumn: 4, user: 'Iwona'}
   ]
 
   export {initColumns, initTasks}
